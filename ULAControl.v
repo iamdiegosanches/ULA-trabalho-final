9fbc17a8ec8720e78a9904dba471672dd00e7686
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2024-01-17 10:16:53 -0300

    Update ULAControl.v

--- ULAControl.v
+++ ULAControl.v
@@ -1,7 +1,9 @@
-module ULAControl(input            clk,
-			      input      [0:1] OpALU,
-				  input      [0:5] funct,
-				  output reg [0:3] inputALU);
+module ULAControl(
+    input           clk,
+    input      [0:1] OpALU,
+    input      [0:5] funct,
+    output reg [0:3] inputALU
+);
 
 always @(negedge clk) begin
     if (OpALU == 2'b00) begin
@@ -11,17 +13,28 @@ always @(negedge clk) begin
         inputALU = 4'b0110; // BEQ
     end
     else if (OpALU == 2'b10) begin
-        case (funct)
-            6'b100000: inputALU = 4'b0010; // ADD
-            6'b100010: inputALU = 4'b0110; // SUB
-            6'b100100: inputALU = 4'b0000; // AND
-            6'b100101: inputALU = 4'b0001; // OR
-            6'b101010: inputALU = 4'b0111; // SLT
-            default: inputALU = 4'b0000;
-        endcase
+        if (funct == 6'b100000) begin
+            inputALU = 4'b0010; // ADD
+        end
+        else if (funct == 6'b100010) begin
+            inputALU = 4'b0110; // SUB
+        end
+        else if (funct == 6'b100100) begin
+            inputALU = 4'b0000; // AND
+        end
+        else if (funct == 6'b100101) begin
+            inputALU = 4'b0001; // OR
+        end
+        else if (funct == 6'b101010) begin
+            inputALU = 4'b0111; // SLT
+        end
+        else begin
+            inputALU = 4'b0000;
+        end
     end
     else begin
         inputALU = 4'b0000;
     end
 end
+
 endmodule
