336d59e1f566aca840992fc98dd1b0a5065efc61
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2024-01-17 10:00:19 -0300

    Update ULAControl.v

--- ULAControl.v
+++ ULAControl.v
@@ -1,24 +1,22 @@
-module ULAControl(
-    input            clk,
-    input      [1:0] OpALU,
-    input      [5:0] funct,
-    output reg [3:0] inputALU
-);
+module ULAControl(input            clk,
+			      input      [0:1] OpALU,
+				  input      [0:5] funct,
+				  output reg [0:3] inputALU);
 
 always @(negedge clk) begin
     if (OpALU == 2'b00) begin
         inputALU = 4'b0010; // LW e SW
     end
     else if (OpALU == 2'b01) begin
-        inputALU = 4'b0110; // Branch Equal
+        inputALU = 4'b0110; // BEQ
     end
     else if (OpALU == 2'b10) begin
         case (funct)
-            6'b100000: inputALU = 4'b0010; // add
-            6'b100010: inputALU = 4'b0110; // subtract
+            6'b100000: inputALU = 4'b0010; // ADD
+            6'b100010: inputALU = 4'b0110; // SUB
             6'b100100: inputALU = 4'b0000; // AND
             6'b100101: inputALU = 4'b0001; // OR
-            6'b101010: inputALU = 4'b0111; // set on less than
+            6'b101010: inputALU = 4'b0111; // SLT
             default: inputALU = 4'b0000;
         endcase
     end
