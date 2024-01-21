d1229ed8c2f96ea137f1d188300dc419ab4eb9b4
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2024-01-21 13:03:04 -0300

    Update ULA.v

--- ULA.v
+++ ULA.v
@@ -5,18 +5,18 @@ module ULA (input         clk,
 			output reg [0:31] outputULA);
 
     always @(posedge clk) begin
-        if (inputULA == 4'b0000) begin
-            // AND
-            outputULA <= a & b;
-        end else if (inputULA == 4'b0001) begin
-            // OR
-            outputULA <= a | b;
-        end else if (inputULA == 4'b0010) begin
+        if (inputULA == 4'b0010) begin
             // ADD
             outputULA <= a + b;
         end else if (inputULA == 4'b0110) begin
             // SUB
             outputULA <= a - b;
+        end else if (inputULA == 4'b0000) begin
+            // AND
+            outputULA <= a & b;
+        end else if (inputULA == 4'b0001) begin
+            // OR
+            outputULA <= a | b;
         end else if (inputULA == 4'b0111) begin
             // SLT
             outputULA <= (a < b) ? 1 : 0;
@@ -24,5 +24,4 @@ module ULA (input         clk,
             outputULA <= 0;
         end
     end
-
 endmodule
